68c684b5fbaa888154997c41aa1fe09a960fcae6
Pearl Leff <leffpb@gmail.com>
2020-07-20 00:30:16 -0400

    Clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,38 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
         concat: {   
             dist: {
                 src: [ 'src/js/lib/*', 'src/js/*.js' ],
                 dest: 'src/production.js',
             }
+        }, 
+        watch: {
+            scripts: {
+                files: ['src/js/*.js'],
+                tasks: [ 'concat' ],
+                options: {
+                    spawn: false,
+                }
+            }
+        }, 
+        jslint: {
+            popup: {
+                src: [ 
+                    'src/js/*.js',
+                ],
+                directives: {
+                    predef: [
+                        'jQuery'
+                    ]
+                }
+            }
         }
-        // , 
-        // uglify: {
-        //     build: {
-        //         src: 'src/production.js',
-        //         dest: 'src/production.min.js'
-        //     }
-        // } 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat')
+    grunt.loadNpmTasks('grunt-contrib-watch') 
+    grunt.loadNpmTasks('grunt-jslint');
 
-    grunt.registerTask('default', ['concat']); // , 'uglify'
-};
+    grunt.registerTask('default', ['concat']) 
+}
